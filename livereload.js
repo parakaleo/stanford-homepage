afd19bd0d3411b8194f28b55f0801384ecf48752
Allan Jiang <origami789@gmail.com>
2017-12-01 14:28:35 -0800

    progress...

--- livereload.js
+++ livereload.js
@@ -5,7 +5,9 @@ var server = connect();
 
 server.use(static(__dirname + '/'));
 
-server.listen(8010);
+server.listen(8010, function() {
+  console.log('listening on port 8010')
+});
 
 var livereload = require('livereload');
 var lrserver = livereload.createServer();
