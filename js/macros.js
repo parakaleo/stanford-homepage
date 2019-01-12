bff9442a1d19b4abd05e459da1f4d2cac116ac68
Allan Jiang <origami789@gmail.com>
2019-01-12 13:20:41 -0800

    fix room

--- js/macros.js
+++ js/macros.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $(".course-weekday").text("Tuesday")
   $(".course-time").text("7-9pm")
   $(".quarter-year").text("Winter 2019")
-  $(".course-location").html("Thornton 207 <a style='color:#0085a1' target='_blank' href='https://campus-map.stanford.edu/?id=04-720&lat=37.4255553&lng=-122.17370443&zoom=17&srch=thornton'>(map)</a>")
+  $(".course-location").html("Thornton 217 <a style='color:#0085a1' target='_blank' href='https://campus-map.stanford.edu/?id=04-720&lat=37.4255553&lng=-122.17370443&zoom=17&srch=thornton'>(map)</a>")
   $(".course-duration").text("2-year")
 });
