bf0a7cf949942e7bcc9ad9f24169116f6c451a47
Allan Jiang <origami789@gmail.com>
2019-09-19 21:12:54 -0700

    update site for fall 2019

--- js/macros.js
+++ js/macros.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $(".course-weekday").text("Tuesday")
+  $(".course-weekday").text("Monday")
   $(".course-time").text("7-9pm")
-  $(".quarter-year").text("Winter 2019")
-  $(".course-location").html("Thornton 207 <a style='color:#0085a1' target='_blank' href='https://campus-map.stanford.edu/?id=04-720&lat=37.4255553&lng=-122.17370443&zoom=17&srch=thornton'>(map)</a>")
+  $(".quarter-year").text("Fall 2019")
+  $(".course-location").html("Old Union 3rd floor Seminar Room 301 <a style='color:#0085a1' target='_blank' href='https://goo.gl/maps/oY2YduWT5R187d7K7'>(map)</a>")
   $(".course-duration").text("2-year")
 });
