d7d53fb8eb27816db994377dc06451227db9a6ab
Allan Jiang <origami789@gmail.com>
2020-02-25 10:25:11 -0800

    winter qtr tweaks

--- js/macros.js
+++ js/macros.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(".course-weekday").text("Monday")
   $(".course-time").text("7-9pm")
-  $(".quarter-year").text("Fall 2019")
+  $(".quarter-year").text("Winter 2020")
   $(".course-location").html("Old Union 3rd floor Seminar Room 301 <a style='color:#0085a1' target='_blank' href='https://goo.gl/maps/oY2YduWT5R187d7K7'>(map)</a>")
   $(".course-duration").text("2-year")
 });
