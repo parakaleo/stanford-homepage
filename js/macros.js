e2a10ba85d1a9f7b5fdee7d70014b96dec5ff679
Allan Jiang <origami789@gmail.com>
2021-09-17 18:07:38 -0700

    prominent email list signup + update sitewide values

--- js/macros.js
+++ js/macros.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $(".course-weekday").text("Monday")
   $(".course-time").text("7:30-9pm")
-  $(".quarter-year").text("Fall 2020")
-  $(".academic-year").text("2020-2021")
+  $(".quarter-year").text("Fall 2021")
+  $(".academic-year").text("2021-2022")
   $(".course-location").html("this <a style='color:#0085a1' target='_blank' href='https://us02web.zoom.us/j/9598309206'>Zoom Link</a>")
   $(".course-duration").text("1-year")
   $(".student-officers").html(`
-    <br>Gloria Yi (gloria.j.yi &lt;at&gt; stanford.edu)
-    <br>Allan Jiang (jiangts &lt;at&gt; stanford.edu)
     <br>Jesse Doan (jdoan21 &lt;at&gt; stanford.edu)
+    <br>Irena Gao (igao &lt;at&gt; stanford.edu)
+    <br>Allan Jiang (jiangts &lt;at&gt; stanford.edu)
   `)
 
   switch (location.pathname) {
