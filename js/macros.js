6cc0d4b35ffb56880cb5d549a20830b51c06535d
Allan Jiang <jiangtsa@gmail.com>
2022-09-26 23:07:08 -0700

    remove time-related updates...

--- js/macros.js
+++ js/macros.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   $(".course-weekday").text("Monday")
   $(".course-time").text("7-9pm")
-  $(".quarter-year").text("Fall 2022")
-  // $(".academic-year").text("2021-2022")
   $(".course-location").html("Old Union 3rd floor, room 301")
   $(".course-duration").text("1-year")
   $(".student-officers").html(`
@@ -25,12 +23,23 @@ $(document).ready(function() {
   }
   var d = new Date()
   var t1 = new Date('1974-09-15')
+  // var autwin = new Date('1974-01-01')
+  // var winspr = new Date('1974-03-15')
   var ministry_years = d.getFullYear() - t1.getFullYear()
   var academic_year = d.getFullYear()
   if(d.setFullYear(t1.getFullYear()) < t1) {
     ministry_years -= 1
     academic_year -= 1
   }
+  var month = d.getMonth() + 1
+  var day = d.getDate()
+  var quarter = (month > 9 || (month === 9 && day >= 15)) ?
+    'Fall' :
+    ((month >= 1 && month < 3) || (month === 3 && day <= 15)) ?
+    'Winter' :
+    ((month > 3 && month < 6) || (month === 3 && day > 15) || (month === 6 && day <= 15)) ?
+    'Spring' : 'Summer'
+  $(".quarter-year").text(quarter + ' ' + d.getFullYear())
   $(".academic-year").text(academic_year + '-' + (academic_year+1))
   $(".ministry-years").text(ministry_years)
   $(".ministry-years.ordinal").text(ministry_years + nth(ministry_years))
